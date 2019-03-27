1846adcdf0b95000d26b46e5c228d55a82bee4de
jibl <jibl@poste-devops-jbl-16gbram.pegasus.io>
2019-03-27 21:40:24 +0100

    amélioration, avec l'idée d'en faire un outil de tests réseau, à la limite entre soft et infra

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # The laby server
+
 Is just a simple HTTP server :
 
 * statically serving files in the `laby/www` folder
-* providing a customizable endpoint at /LABY_HOST/
+* providing a REST API endpoint at http://`$LABY_HOST`/antenna/api/flowers :
+  * This endpoint is what I call My _antenna_, which I customize ad redeploy on-the-fly, to make actual devops tests
+  * I especially use it in docker environements, to determine whether the problem came from the software, from the infrastructure, or even better, from both
 
 
 # Install it
@@ -17,33 +20,30 @@ export URI_DE_CE_REPO=https://github.com/Jean-Baptiste-Lasselle/laby
 mkdir -p ~/laby
 cd ~/laby
 
-git clone $URI_DE_CE_REPO . 
+git clone $URI_DE_CE_REPO .
 ```
 
 
 # Configure and Run it
 
-Inside the 
+Inside the
 
 ```bash
 # sets Laby's port number
-export LABY_PORT=3000 
-# sets Laby's 
-export LABY_HOST=0.0.0.0 
-export LABY_BACKLOG=500 
-npm run start
+export LABY_PORT=3000
+# sets Laby's
+export LABY_HOST=0.0.0.0
+# Ever heard about servers' Backlog? Google that and come back on that config param
+export LABY_BACKLOG=500
+# npm run start
+npm start
 ```
 
 
-**_What you get 
+**_What you get_**
 
 
 
 <!--
 ![screeshow 3](ccc)
 -->
-
-
-
-
-
--- laby.js
+++ laby.js
@@ -1,10 +1,11 @@
 var express = require("express");
+var antennaFlowersAPIRouter = require('./laby/endpoints/antenna-flower-json-api.js');
 
-const hostname = process.env.OMEGA_HOST;
-const numeroPortMonAppli = process.env.OMEGA_PORT;
+const hostname = process.env.LABY_HOST;
+const numeroPortMonAppli = process.env.LABY_PORT;
 // cf. `baclklog` argument of listen method, setting maximum number of pending requests
 //  I'll set that to 500 here
-const nombreMaximalDeRequetesEnTraitement = process.env.OMEGA_BACKLOG;
+const nombreMaximalDeRequetesEnTraitement = process.env.LABY_BACKLOG;
 // const nombreMaximalDeRequetesEnTraitement = process.env.BACKLOG;
 
 console.log("vérification hostname : " + hostname);
@@ -13,33 +14,41 @@ console.log("vérification BACKLOG : " + nombreMaximalDeRequetesEnTraitement);
 
 /**
  *
- *   OMEGA APP
- * 
+ *   LABY APP
+ *
  ***/
-var omega = express();
+var laby = express();
 
 /**
  *
- *   OMEGA APP STATIC ROUTES
- * 
+ *   laby APP STATIC ROUTES
+ *
  ***/
-omega.use(express.static('grapes'));
-omega.use(express.static('workspace'));
+laby.use(express.static('laby/www'));
 
+/**
+ *
+ *   laby ANTENNA ROUTES
+ *
+ ***/
 
-var storage_endpoint = require('./omega/storage-endpoint');
-
-omega.use('/storage', storage_endpoint);
-
+/**
+ * a CRUD for something like an online flower shopping REST API
+ **/
+antenna.use('/antenna/api/flowers', antennaFlowersAPIRouter);
+/**
+ * a CRUD for something like the website of a flower shop vitirne site, aka no shop features, just where to find about and all that sh**t
+ **/
+antenna.use('/antenna/web/flowers', antennaFlowersAPIRouter);
 
 
 // https://nodejs.org/api/net.html#net_server_listen
-// server.listen([port[, host[, backlog]]][, callback]) for TCP servers 
+// server.listen([port[, host[, backlog]]][, callback]) for TCP servers
 // baclklog = maximum number of pending requests, I'll set that to 500 here
-var server = omega.listen(numeroPortMonAppli, hostname, nombreMaximalDeRequetesEnTraitement, function(){
+var server = laby.listen(numeroPortMonAppli, hostname, nombreMaximalDeRequetesEnTraitement, function(){
     var numport = server.address().port;
     var nomReseau = server.address().address;
     var nbMaxReq = 511; // 511 is the default server backlog limit // server.backlog();
-    
-    console.log("Omega Server started at http://" + nomReseau + ":%s", numport + " [nbMaxReq] " + nbMaxReq);
+
+    console.log(" Laby Server started at http://" + nomReseau + ":%s", numport + " [nbMaxReq] " + nbMaxReq);
 });
